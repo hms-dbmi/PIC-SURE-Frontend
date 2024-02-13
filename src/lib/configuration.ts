938c6d00b5432f76e99d8ffc48be54e9c89422b7
Samantha <samantha.piatt@childrens.harvard.edu>
2024-02-13 16:00:22 -0500

    [ALS-5894] Add help page button links (#6)

---
+++ src/lib/configuration.ts
@@ -0,0 +1,30 @@
+export const branding = {
+  help: {
+    links: [
+      {
+        title: 'User Guide',
+        description: 'Complete user manual for seamless navigation and utilization.',
+        icon: 'fa-solid fa-book fa-5x',
+        url: '/help/dummy?title=' + encodeURI('User Guide')
+      },
+      {
+        title: 'Video Library',
+        description: "Example 'how-to' video demonstrations.",
+        icon: 'fa-solid fa-tv fa-5x',
+        url: '/help/dummy?title=' + encodeURI('Video Library')
+      },
+      {
+        title: 'Request Assistance',
+        description: 'Need help? Submit a service desk ticket, we are here to help!',
+        icon: 'fa-solid fa-hands-holding-circle fa-5x',
+        url: '/help/dummy?title=' + encodeURI('Request Assistance')
+      },
+      {
+        title: 'PIC-Sure Website',
+        description: 'Check out the PIC-Sure website for information.',
+        icon: 'fa-solid fa-circle-info fa-5x',
+        url: '/help/dummy?title=' + encodeURI('PIC-Sure Website')
+      }
+    ]
+  }
+};
---
+++ tests/routes/help/test.ts
@@ -0,0 +1,11 @@
+import { expect, test } from '@playwright/test';
+import { branding } from '../../../src/lib/configuration';
+
+test.describe('Help page', () => {
+  branding.help.links.forEach(({ title }) => {
+    test(`Has expected link to ${title}`, async ({ page }) => {
+      await page.goto('/help');
+      await expect(page.getByText(title, { exact: true })).toBeVisible();
+    });
+  });
+});
