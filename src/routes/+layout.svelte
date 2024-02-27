c799a52b9ad0a5fe6650e89169c1df64583b1c3e
James <Jamestp19@gmail.com>
2024-02-27 15:02:50 -0500

    [ALS-5983] Add a temp session modal

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,50 @@
 <script lang="ts">
-  import { AppShell } from '@skeletonlabs/skeleton';
+  import { AppShell, initializeStores, Modal, Toast, getModalStore, type ModalSettings } from '@skeletonlabs/skeleton';
   import Navigation from '$lib/navigation.svelte';
   import '@fortawesome/fontawesome-free/css/all.min.css';
   import '../app.postcss';
+  import { onMount } from 'svelte';
+  initializeStores();
+  let modalProps: Record<string, unknown> = {
+    buttonPositive: 'variant-filled-primary',
+  };
+  const modalStore = getModalStore();
+  onMount(() => {
+    document.body.classList.add("started");
+    checkForSession();
+  });
   // // Floating UI for Popups
   // import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
   // import { storePopup } from '@skeletonlabs/skeleton';
   // storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
+  function checkForSession() {
+    if (!sessionStorage.getItem('token')) {
+      const modal: ModalSettings = {
+      type: 'prompt',
+      // Data
+      title: 'Enter Long Term Token',
+      body: 'Provide your long term token below.',
+      // Populates the input value and attributes
+      value: 'Token here...',
+      valueAttr: { type: 'text', required: true },
+      // Returns the updated response value
+      response: (r: string) => {
+        sessionStorage.setItem('token', r);
+      },
+    };
+    modalStore.trigger(modal);
+    }
+  }
 </script>
 
+<Toast />
+<Modal {...modalProps} />
 <AppShell>
   <svelte:fragment slot="header">
     <Navigation />
+    <hr class="!border-t-2" />
   </svelte:fragment>
-  <hr class="!border-t-2" />
   <!-- (sidebarLeft) -->
   <!-- (sidebarRight) -->
   <!-- (pageHeader) -->
