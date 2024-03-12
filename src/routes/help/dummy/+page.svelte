e2cf65f803c4de9549a572030e8ce718ae0d4907
James <Jamestp19@gmail.com>
2024-03-12 08:52:04 -0400

    [ALS-6089] Add real links to help menu

--- src/routes/help/dummy/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import Content from '$lib/components/Content.svelte';
-  import { page } from '$app/stores';
-  export const title = $page.url.searchParams.get('title') || 'Dummy';
-</script>
-
-<Content {title}>
-  <p>Some help page content.</p>
-</Content>
