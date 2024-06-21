c58e94bf51d5fc5b02cbc84a24f00980847f65a8
James <Jamestp19@gmail.com>
2024-06-21 09:39:17 -0400

    init page

---
+++ src/routes/(picsure)/(public)/explorer/export/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import AngleButton from "$lib/components/buttons/AngleButton.svelte";
+    import ExportStepper from "$lib/components/explorer/export/ExportStepper.svelte";
+import { AppBar } from "@skeletonlabs/skeleton";
+
+export let query: any;
+
+</script>
+
+
+<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+  <svelte:fragment slot="lead">
+    <AngleButton angle="left" variant="ringed" color="primary" name="Back" on:click={() => history.back()}>Back to Cohort Builder</AngleButton>
+  </svelte:fragment>
+	Export Data for Research Analysis
+</AppBar>
+<section class="flex justify-center items-center w-full h-full">
+  <ExportStepper {query}  />
+</section>
+
+<style>
+</style>
