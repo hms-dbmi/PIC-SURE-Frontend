6e096d2f08f3fd90c585bd1fb7ef6f9390ba93cf
Samantha <samantha.piatt@childrens.harvard.edu>
2024-06-28 14:39:19 -0400

    [ALS-6667] Footer changes (#81)

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,20 +1,40 @@
 <script lang="ts">
   import { LightSwitch } from '@skeletonlabs/skeleton';
 
+  import { page } from '$app/stores';
+
   import { branding } from '$lib/configuration';
-  export let showSitemap = branding.footer.addSitemap;
+  import { user } from '$lib/stores/User';
+  export let showSitemap: boolean = branding.footer.showSitemap;
+
+  $: hideSitemap =
+    !showSitemap ||
+    branding.footer.excludeSitemapOn.find((hide) => $page.url.pathname.includes(hide));
+
+  $: sitemap = branding.sitemap.map((section) => ({
+    ...section,
+    show: !section.privilege || ($user.privileges && $user.privileges.includes(section.privilege)),
+  }));
 </script>
 
-{#if showSitemap && branding.sitemap.length > 0}
+{#if !hideSitemap && branding.sitemap.length > 0}
   <div id="sitemap-footer">
     <div class="flex flex-wrap place-content-center">
-      {#each branding.sitemap as section}
-        <ul class="basis-1/4">
-          <li class="font-bold">{section.category}</li>
-          {#each section.links as link}
-            <li><a target={link.newTab ? '_blank' : '_self'} href={link.url}>{link.title}</a></li>
-          {/each}
-        </ul>
+      {#each sitemap as section}
+        {#if section.show}
+          <ul class="basis-1/8">
+            <li class="font-bold text-center">{section.category}</li>
+            {#each section.links as link}
+              <li class="text-center">
+                <a
+                  target={link.newTab ? '_blank' : '_self'}
+                  href={link.url}
+                  class="hover:text-primary-600-300-token hover:underline">{link.title}</a
+                >
+              </li>
+            {/each}
+          </ul>
+        {/if}
       {/each}
     </div>
   </div>
