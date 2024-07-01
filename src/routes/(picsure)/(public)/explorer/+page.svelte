16aad3264509f2467c7d987e7eb3ba3751fdad73
James <Jamestp19@gmail.com>
2024-07-01 09:47:43 -0400

    [ALS-6269] Update search to call dictionary

--- src/routes/(picsure)/(public)/explorer/+page.svelte
+++ src/routes/(picsure)/(public)/explorer/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { onMount } from 'svelte';
-  import { Accordion, AccordionItem, ProgressBar } from '@skeletonlabs/skeleton';
+  import { ProgressRadial } from '@skeletonlabs/skeleton';
 
   import { page } from '$app/stores';
   import { goto } from '$app/navigation';
@@ -9,7 +9,6 @@
 
   import Actions from '$lib/components/explorer/cell/Actions.svelte';
   import Content from '$lib/components/Content.svelte';
-  import Checkbox from '$lib/components/explorer/Checkbox.svelte';
   import Datatable from '$lib/components/datatable/Table.svelte';
   import ErrorAlert from '$lib/components/ErrorAlert.svelte';
   import AddFilterComponent from '$lib/components/explorer/AddFilter.svelte';
@@ -19,10 +18,13 @@
 
   import SearchStore from '$lib/stores/Search';
   import { setComponentRegistry } from '$lib/stores/ExpandableRow';
-  let { tags, searchTerm, searchResults, search } = SearchStore;
+  import FacetSideBar from '$lib/components/explorer/FacetSideBar.svelte';
+  let { searchTerm, searchResults, search, selectedFacets } = SearchStore;
 
   let searchInput = $page.url.searchParams.get('search') || $searchTerm || '';
   let searchPromise: Promise<void> = search(searchInput);
+  let resultsPage = 0;
+  let pageSize = 10;
 
   const columns = [
     { dataElement: 'name', label: 'Variable Name', sort: true },
@@ -53,58 +55,64 @@
 
   function updateSearch() {
     goto(searchInput ? `/explorer?search=${searchInput}` : '/explorer', { replaceState: true });
-    searchPromise = search(searchInput);
+    searchPromise = search(searchInput, resultsPage, pageSize);
   }
+
+  selectedFacets.subscribe(() => {
+    console.log('selectedFacets changed, now getting facets with: ', $searchTerm, $selectedFacets);
+    searchPromise = search(searchInput, resultsPage, pageSize);
+  });
 </script>
 
 <svelte:head>
   <title>{branding.applicationName} | Explorer</title>
 </svelte:head>
 <Content full>
-  {#await searchPromise}
-    <h3 class="text-left">Loading</h3>
-    <ProgressBar animIndeterminate="anim-progress-bar" />
-  {:then}
-    <div class="grid grid-cols-4 gap-6">
-      <div id="search-tags">
-        {#if $tags.length > 0}
-          <Accordion autocollapse>
-            {#each $tags as tagType, index}
-              <AccordionItem open={index === 0}>
-                <svelte:fragment slot="summary">{tagType.title}</svelte:fragment>
-                <svelte:fragment slot="content">
-                  {#each tagType.tags as tag}
-                    <Checkbox type={tagType.title} tag={tag.name} state={tag.state} />
-                  {/each}
-                </svelte:fragment>
-              </AccordionItem>
-            {/each}
-          </Accordion>
-        {:else}
-          <p>No Tags to display.</p>
-        {/if}
-      </div>
-      <div class="col-span-3">
-        <div id="search-bar" class="flex gap-2 mb-6">
-          <div class="flex-auto">
-            <Searchbox bind:searchTerm={searchInput} search={updateSearch} />
-          </div>
-          <div class="flex-none">
-            {#if features.genomicFilter}
-              <a
-                data-testid="genomic-filter-btn"
-                class="btn variant-ghost-primary hover:variant-filled-primary"
-                href="/explorer/genome-filter">Genomic Filtering</a
-              >
-            {/if}
-          </div>
+  <section id="search-container" class="grid grid-cols-4 gap-6">
+    <div id="facets-col">
+      <FacetSideBar />
+    </div>
+    <div id="search-results-col" class="col-span-3">
+      <div id="search-bar" class="flex gap-2 mb-6">
+        <div class="flex-auto">
+          <Searchbox bind:searchTerm={searchInput} search={updateSearch} />
         </div>
-        <Datatable {tableName} data={$searchResults} {columns} {cellOverides} />
+        <div class="flex-none">
+          {#if features.genomicFilter}
+            <a
+              data-testid="genomic-filter-btn"
+              class="btn variant-ghost-primary hover:variant-filled-primary"
+              href="/explorer/genome-filter">Genomic Filtering</a
+            >
+          {/if}
+          <button
+            type="button"
+            class="btn variant-ghost-error hover:variant-filled-error"
+            aria-label="You are on the reset button"
+            disabled={!searchInput && $selectedFacets.length === 0}
+            on:click={() => {
+              searchInput = '';
+              searchTerm.set('');
+              searchResults.set([]);
+              selectedFacets.set([]);
+              goto('/explorer');
+            }}
+          >
+            Reset
+          </button>
+        </div>
+      </div>
+      <div>
+        {#await searchPromise}
+          <ProgressRadial />
+        {:then}
+          <Datatable {tableName} data={$searchResults} {columns} {cellOverides} />
+        {:catch}
+          <ErrorAlert title="API Error">
+            <p>Something went wrong when sending your request.</p>
+          </ErrorAlert>
+        {/await}
       </div>
     </div>
-  {:catch}
-    <ErrorAlert title="API Error">
-      <p>Something went wrong when sending your request.</p>
-    </ErrorAlert>
-  {/await}
+  </section>
 </Content>
