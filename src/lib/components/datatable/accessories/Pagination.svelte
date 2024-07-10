2443def04818f3db2e15e7e7a0e3475f92e7cb8b
Samantha <samantha.piatt@childrens.harvard.edu>
2024-07-10 15:22:21 -0400

    [ALS-6241] Design updates for variant explorer and datatables (#95)

--- src/lib/components/datatable/accessories/Pagination.svelte
+++ src/lib/components/datatable/accessories/Pagination.svelte
@@ -7,19 +7,13 @@
 </script>
 
 <!-- Desktop buttons -->
-<section class="btn-group variant-ghost-surface [&>*+*]:border-surface-500 h-10 hidden lg:block">
-  <button
-    type="button"
-    class="hover:variant-soft-primary"
-    class:disabled={$pageNumber === 1}
-    on:click={() => handler.setPage('previous')}
-  >
+<section class="btn-group-custom h-10 hidden lg:block">
+  <button type="button" disabled={$pageNumber === 1} on:click={() => handler.setPage('previous')}>
     ←
   </button>
   {#each $pages as page}
     <button
       type="button"
-      class="hover:variant-soft-primary"
       class:active={$pageNumber === page}
       class:ellipse={page === null}
       on:click={() => handler.setPage(page)}
@@ -29,8 +23,7 @@
   {/each}
   <button
     type="button"
-    class="hover:variant-soft-primary"
-    class:disabled={$pageNumber === $pageCount}
+    disabled={$pageNumber === $pageCount}
     on:click={() => handler.setPage('next')}
   >
     →
