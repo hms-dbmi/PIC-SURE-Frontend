4b2d73c7c3bcfa388fe7271ccc056f02deb2ef96
Samantha <samantha.piatt@childrens.harvard.edu>
2024-07-17 15:21:00 -0400

    [ALS-5477] Add terms modal and default text (#102)
    - Also fix stats loading width bug.

--- src/lib/components/Stats.svelte
+++ src/lib/components/Stats.svelte
@@ -1,55 +1,52 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
   import { ProgressRadial } from '@skeletonlabs/skeleton';
 
   import { getOrApi, ERROR_VALUE } from '$lib/stores/Stats';
   import { branding } from '$lib/configuration';
   import type { Value } from '$lib/models/Value';
 
-  let stats: Value[] = [];
+  let stats: Value[] = branding.landing.stats.map((stat) => ({
+    title: stat,
+    value: ERROR_VALUE,
+    loading: true,
+  }));
+  let width = branding.landing.stats.length;
+  $: hasError = stats.find((stat) => !stat.loading && stat.value === ERROR_VALUE) || false;
 
-  let getValues = async function () {
+  onMount(async () => {
     stats = await Promise.all(
       branding.landing.stats.map(async (stat) => {
         let value = await getOrApi(stat);
-        return { value, title: stat } as Value;
+        return { value, title: stat, loading: false } as Value;
       }),
     );
-  };
-  let width = branding.landing.stats.length;
-
-  $: hasError = stats.find((stat) => stat.value === ERROR_VALUE) || false;
+  });
 
   /* eslint-disable svelte/no-at-html-tags */
   // @html branding.landing.explanation is from a static file and will only be populated by staff.
 </script>
 
 <section class="stats flex flex-row flex-wrap justify-evenly items-center w-full px-4 gap-y-4">
-  {#await getValues()}
-    {#each branding.landing.stats as stat}
-      <div id="value-{stat}" class="flex flex-col justify-center items-center">
-        <ProgressRadial
-          width="w-10"
-          meter="stroke-surface-50 dark:stroke-surface-900"
-          track="stroke-secondary-500/30"
-          value={undefined}
-        />
-        <p>{stat}</p>
+  {#each stats as stat}
+    <div class="flex flex-col w-1/{width}">
+      <div data-testid="value-{stat.title}" class="flex flex-col justify-center items-center">
+        {#if stat.loading}
+          <ProgressRadial
+            width="w-10"
+            meter="stroke-surface-50 dark:stroke-surface-900"
+            track="stroke-secondary-500/30"
+            value={undefined}
+          />
+        {:else if stat.value === ERROR_VALUE}
+          <i class="fa-solid fa-circle-exclamation"></i>
+        {:else}
+          {stat.value}
+        {/if}
       </div>
-    {/each}
-  {:then}
-    {#each stats as stat}
-      <div class="flex flex-col w-1/{width}">
-        <div data-testid="value-{stat.title}" class="flex flex-col justify-center items-center">
-          {#if stat.value === ERROR_VALUE}
-            <i class="fa-solid fa-circle-exclamation"></i>
-          {:else}
-            {stat.value}
-          {/if}
-        </div>
-        <p>{stat.title}</p>
-      </div>
-    {/each}
-  {/await}
+      <p>{stat.title}</p>
+    </div>
+  {/each}
   <div class="w-3/4">
     {@html branding.landing.explanation}
   </div>
