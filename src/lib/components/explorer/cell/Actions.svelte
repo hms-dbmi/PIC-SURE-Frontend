44fb5f1250a327218b098cfd051692bef968b03b
James <Jamestp19@gmail.com>
2024-08-20 16:00:11 -0400

    fix linting

--- src/lib/components/explorer/cell/Actions.svelte
+++ src/lib/components/explorer/cell/Actions.svelte
@@ -68,7 +68,7 @@
 -->
 <button
   type="button"
-  title="{isExported ? 'Remove from Analysis' : 'Add for Analysis'}"
+  title={isExported ? 'Remove from Analysis' : 'Add for Analysis'}
   class="btn-icon-color"
   on:click|stopPropagation={insertExportContent}
 >
