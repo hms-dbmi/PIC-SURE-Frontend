1320caaf41b44e0f14fed6bbe52a5525ce1ef694
James <Jamestp19@gmail.com>
2024-09-16 10:22:41 -0400

    [ALS-7040] Footer changes to fit more links

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -19,6 +19,7 @@ export interface SiteMapConfig {
 
 export interface FooterConfig {
   showSitemap: boolean;
+  showTerms: boolean;
   excludeSitemapOn: string[];
   links: Array<{
     title: string;
