ee84f806fb90817db5e9e2262481679a4fa5daae
Ryan Amari <ramari16@gmail.com>
2024-09-19 10:37:29 -0400

    ALS-7300: Copy and layout changes for BDC analyze page

--- src/routes/(picsure)/(authorized)/analyze/+page.svelte
+++ src/routes/(picsure)/(authorized)/analyze/+page.svelte
@@ -11,11 +11,24 @@
   <title>{branding.applicationName} | API</title>
 </svelte:head>
 
-<Content title="Analyze">
+<Content title="Prepare for Analysis with the PIC-SURE API">
   <section class="flex flex-col gap-8">
     <p class="mt-4">
-      To start your analysis, use the following code to connect to PIC-SURE. Note that you will need
-      your personal access token below to complete the connection.
+      The PIC-SURE Application Programming Interface (API) can be used in an analysis environment of your choice.
+      This API is available in both Python and R coding languages.
+    </p>
+    <p>
+      To connect to the PIC-SURE Application Programming Interface (API), you will need your personal access token.
+      Copy your token and save as a text file called “token.txt” in the working directory of your chosen analysis
+      workspace, such as BioData Catalyst Powered by Seven Bridges or BioData Catalyst Powered by Terra.
+    </p>
+    <div class="flex justify-center">
+      <UserToken />
+    </div>
+    <p>
+      To start your analysis, copy and execute the following code in an analysis environment, such as BioData Catalyst
+      Powered by Seven Bridges or BioData Catalyst Powered by Terra, to connect to the PIC-SURE API. Note that you will
+      need your personal access token to complete the connection.
     </p>
     <TabGroup class="card p-4">
       <Tab bind:group={tabSet} name="python" value={0}>Python</Tab>
@@ -26,48 +39,46 @@
             language="python"
             lineNumbers={true}
             buttonCopied="Copied!"
-            code={`# Requires python 3.7 or later
+            code={`# Requires python 3.6 or later
 import sys
 import pandas as pd
 import matplotlib.pyplot as plt
-!{sys.executable} -m pip install --upgrade --force-reinstall git+https://github.com/hms-dbmi/pic-sure-python-adapter-hpds.git
+# BDC Powered by Terra users uncomment the following line to specify package install location
+# sys.path.insert(0, r"/home/jupyter/.local/lib/python3.7/site-packages")
 !{sys.executable} -m pip install --upgrade --force-reinstall git+https://github.com/hms-dbmi/pic-sure-python-client.git
-
-import PicSureHpdsLib
+!{sys.executable} -m pip install --upgrade --force-reinstall git+https://github.com/hms-dbmi/pic-sure-python-adapter-hpds.git
+!{sys.executable} -m pip install --upgrade --force-reinstall git+https://github.com/hms-dbmi/pic-sure-biodatacatalyst-python-adapter-hpds.git
 import PicSureClient
-
+import PicSureBdcAdapter
+# Set up connection to PIC-SURE API
+PICSURE_network_URL = "https://picsure.biodatacatalyst.nhlbi.nih.gov/picsure"
 token_file = "token.txt"
-with open(token_file, "r") as f:
-my_token = f.read()
 
-connection = PicSureClient.Client.connect(url = PICSURE_network_URL, token = my_token)`}
+with open(token_file, "r") as f:
+    my_token = f.read()
+
+bdc = PicSureBdcAdapter.Adapter(PICSURE_network_URL, my_token)`}
           ></CodeBlock>
         {:else if tabSet === 1}
           <CodeBlock
             language="r"
             lineNumbers={true}
             code={`# Requires R 3.4 or later
-install.packages("devtools")
-
+### Uncomment this code if you are not using the PIC-SURE environment in *BDC-Seven Bridges*, or if you do not have all the necessary dependencies installed.
+#install.packages("devtools")
+Sys.setenv(TAR = "/bin/tar")
+options(unzip = "internal")
 devtools::install_github("hms-dbmi/pic-sure-r-adapter-hpds", ref="main", force=T, quiet=FALSE)
 library(dplyr)
-
-PICSURE_network_URL = "https://nhanes.hms.harvard.edu/picsure"
+PICSURE_network_URL = "https://picsure.biodatacatalyst.nhlbi.nih.gov/picsure"
 token_file <- "token.txt"
 token <- scan(token_file, what = "character")
 session <- picsure::bdc.initializeSession(PICSURE_network_URL, token)
-session <- picsure::bdc.setResource(session = session)`}
+session <- picsure::bdc.setResource(session = session,  resourceName = "AUTH")`}
           ></CodeBlock>
         {/if}
       </svelte:fragment>
     </TabGroup>
-    <p>
-      Copy your personal access token and save as a text file called “token.txt” in the same working
-      directory to execute the code above.
-    </p>
-    <div class="flex justify-center">
-      <UserToken />
-    </div>
   </section>
   <section id="info-cards" class="w-full flex flex-wrap flex-row justify-center mt-6">
     {#each branding.apiPage.cards as card}
