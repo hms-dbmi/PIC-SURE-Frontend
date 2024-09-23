9074f8e27dda53690156bc8f5365d8bae0d6b19b
James <Jamestp19@gmail.com>
2024-09-23 09:44:38 -0400

    Init new info screen

--- src/lib/components/explorer/ResultInfoComponent.svelte
+++ src/lib/components/explorer/ResultInfoComponent.svelte
@@ -1,29 +1,50 @@
 <script lang="ts">
   import type { SearchResult } from '$lib/models/Search';
+    import { getConceptDetails } from '$lib/services/dictionary';
+    import { ProgressRadial } from '@skeletonlabs/skeleton';
 
-  export let data: SearchResult = {} as SearchResult;
+  export let searchResult: SearchResult;
+
+  let detailPromise = getConceptDetails(searchResult.conceptPath, searchResult.dataset);
+
+
 </script>
 
 <div class="card min-h-60 p-4">
-  <section class="card-body">
-    <ul class="break-all">
-      {#if data.display}
-        <li>
-          <strong>Name</strong>: {data.display}
-        </li>
+  <div class="card-body">
+    {#await detailPromise}
+      <ProgressRadial />
+    {:then searchResultDetail } 
+      <section class="flex flex-col w-3/4 p-4">
+        <h3>Variable Information</h3>
+        <div class="w-full flex flex-row justify-between">
+          <div><span class="label">Name:</span> {searchResultDetail.display}</div>
+          <div><span class="label">Accession:</span> {searchResultDetail.name}</div>
+          <div><span class="label">Type:</span> {searchResultDetail.type}</div>
+        </div>
+        <div><span class="label">Description:</span> {searchResultDetail.description}</div>
+      </section>
+      {#if searchResultDetail.table}
+      <section class="flex flex-col w-3/4 p-4">
+        <h3>Dataset Information</h3>
+        <div class="w-full flex flex-row justify-between">
+          <div><span class="label">Name:</span> {searchResultDetail.display}</div>
+          <div><span class="label">Accession:</span> {searchResultDetail.name}</div>
+        </div>
+        <div><span class="label">Description:</span> {searchResultDetail.description}</div>
+      </section>
       {/if}
-      {#if data.description}
-        <li>
-          <strong>Description</strong>: {data.description}
-        </li>
+      {#if searchResultDetail.study}
+      <section class="flex flex-col w-3/4 p-4">
+        <h3>Study Information</h3>
+        <div class="w-full flex flex-row justify-between">
+          <div><span class="label">Study Name:</span> {searchResultDetail.display}</div>
+          <div><span class="label">Study Accession:</span> {searchResultDetail.name}</div>
+        </div>
+      </section>
       {/if}
-      {#if data.type}
-        <li>
-          <strong>Type</strong>: {data.type}
-        </li>
-      {/if}
-    </ul>
-  </section>
+    {/await}
+  </div>
 </div>
 
 <style>
