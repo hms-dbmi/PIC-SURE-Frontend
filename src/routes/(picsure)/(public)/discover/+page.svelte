370497cbfdce63eb91e99a1a797862228f1fded3
Ryan Amari <ramari16@gmail.com>
2024-09-26 14:24:41 -0400

    Test some colors

--- src/routes/(picsure)/(public)/discover/+page.svelte
+++ src/routes/(picsure)/(public)/discover/+page.svelte
@@ -39,6 +39,41 @@
         </div>
       </ErrorAlert>
     </section>
+    <section id="discover-error-container2" class="flex gap-9">
+      <aside data-testid="error-alert" class="alert variant-filled-error">
+        <i class="fa-solid fa-triangle-exclamation text-4xl" aria-hidden="true"></i>
+        <div class="alert-message">
+          <h3 class="h3 text-left">Your selected filters contain stigmatizing variables and/or genomic filters, </h3>
+          <h3 class="h3 text-left">which are not supported with Discover</h3>
+          <p>Would you like to remove the invalid filters or go back to explore?</p>
+          <div>
+            <button class="btn variant-ghost" on:click={() => resetQuery()}
+            >Remove Invalid Filters</button
+            >
+            <button class="btn variant-soft" on:click={() => goto('/explorer')}
+            >Back to Explore</button
+            >
+            <button class="btn variant-ringed" on:click={() => goto('/explorer')}
+            >Button 3</button
+            >
+            <button class="btn variant-filled" on:click={() => goto('/explorer')}
+            >Button 4</button
+            >
+          </div>
+          <div class="dark">
+            <button class="btn variant-ghost" on:click={() => resetQuery()}
+            >Remove Invalid Filters</button
+            >
+            <button class="btn variant-soft" on:click={() => goto('/explorer')}
+            >Back to Explore</button
+            >
+            <button class="btn variant-filled" on:click={() => goto('/explorer')}
+            >Button 4</button
+            >
+          </div>
+        </div>
+      </aside>
+    </section>
   {:else}
     <Explorer tourConfig={openTour} />
   {/if}
