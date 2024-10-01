aa5f6ac714e999384cb773a91b7ec597ed644e58
James <Jamestp19@gmail.com>
2024-10-01 17:19:52 -0400

    Update +layout.svelte

--- src/routes/(picsure)/+layout.svelte
+++ src/routes/(picsure)/+layout.svelte
@@ -57,17 +57,18 @@
     !$page.url.pathname.includes('/distributions');
 
   beforeNavigate((nav) => {
+    console.log(nav);
     if ($hasInvalidFilter && nav?.to?.url.pathname.includes('/explorer')) {
-      modalStore.trigger({
-      type: 'component',
-      component: 'modalWrapper',
-      meta: { component: FilterWarning, width: 'w-3/4' },
-      });
-    } else if (($hasGenomicFilter || $hasUnallowedFilter) && nav?.to?.url.pathname.includes('/discover')) {
       modalStore.trigger({
         type: 'component',
         component: 'modalWrapper',
         meta: { component: FilterWarning, width: 'w-3/4' },
+      });
+    } else if (($hasGenomicFilter || $hasUnallowedFilter) && nav?.to?.url.pathname.includes('/discover')) {
+      modalStore.trigger({
+          type: 'component',
+          component: 'modalWrapper',
+          meta: { component: FilterWarning, width: 'w-3/4' },
         });
       }
   });
