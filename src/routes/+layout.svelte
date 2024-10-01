e3c6a1ae516a64832699bbf180fdd16e8f454b70
James <Jamestp19@gmail.com>
2024-10-01 18:22:55 -0400

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,9 +8,31 @@
   import GoogleConsents from '$lib/components/tracking/GoogleConsents.svelte';
   import GoogleTagManger from '$lib/components/tracking/GoogleTagManger.svelte';
   let googleTagManagerId = settings.google.tagManager;
+  import { beforeNavigate } from '$app/navigation';
+  import { hasInvalidFilter, hasGenomicFilter, hasUnallowedFilter } from '$lib/stores/Filter.ts';
+  import { getModalStore } from '@skeletonlabs/skeleton';
+  import FilterWarning from '$lib/components/FilterWarning.svelte';
 
+  
   initializeStores();
   initializeBranding();
+  const modalStore = getModalStore();
+  beforeNavigate((nav) => {
+    console.log(nav);
+    if ($hasInvalidFilter && nav?.to?.url.pathname.includes('/explorer')) {
+      modalStore.trigger({
+        type: 'component',
+        component: 'modalWrapper',
+        meta: { component: FilterWarning, width: 'w-3/4' },
+      });
+    } else if (($hasGenomicFilter || $hasUnallowedFilter) && nav?.to?.url.pathname.includes('/discover')) {
+      modalStore.trigger({
+          type: 'component',
+          component: 'modalWrapper',
+          meta: { component: FilterWarning, width: 'w-3/4' },
+        });
+      }
+  });
 </script>
 
 <main class="w-full h-full">
