c9bf11004770e192a57cf13d25fb2f5ba800b6d9
Ryan Amari <ramari16@gmail.com>
2024-10-01 10:58:15 -0400

    format and lint

--- src/routes/(picsure)/(public)/explorer/+page.svelte
+++ src/routes/(picsure)/(public)/explorer/+page.svelte
@@ -3,29 +3,28 @@
   import Content from '$lib/components/Content.svelte';
   import Explorer from '$lib/components/explorer/Explorer.svelte';
   import authTour from '$lib/assets/authTourConfiguration.json';
-  import {user} from "$lib/stores/User.ts";
-  import {filters} from "$lib/stores/Filter.ts";
-  import {derived, get, type Readable} from "svelte/store";
-  import type {Filter} from "$lib/models/Filter.ts";
-
-
+  import { user } from '$lib/stores/User.ts';
+  import { filters } from '$lib/stores/Filter.ts';
+  import { get } from 'svelte/store';
+  import type { Filter } from '$lib/models/Filter.ts';
 
   function filtersInvalidForUser(): boolean {
     const currentFilters: Filter[] = get(filters);
     const queryScopes: string[] = get(user).queryScopes || [];
-    console.log("query scopes: " + queryScopes);
-    console.log("filters:" + currentFilters);
+    console.log('query scopes: ' + queryScopes);
+    console.log('filters:' + currentFilters);
 
     if (currentFilters.length === 0) {
       return false;
     }
 
-    let hasInvalidFilter : boolean = !!currentFilters.find((filter) => {
-      console.log("Filter description: " + filter.dataset);
+    let hasInvalidFilter: boolean = !!currentFilters.find((filter) => {
+      console.log('Filter description: ' + filter.dataset);
       let filterHasValidQueryScope: boolean = !!queryScopes.find((qs) => {
-        let filterMatchesQueryScope = (filter.dataset || '').length > 0 && qs.indexOf(filter.dataset || 'INVALID FILTER')  >= 0;
+        let filterMatchesQueryScope =
+          (filter.dataset || '').length > 0 && qs.indexOf(filter.dataset || 'INVALID FILTER') >= 0;
         if (filterMatchesQueryScope) {
-          console.log("Filter {" + filter.dataset + "} matches queryScope {" + qs + "}");
+          console.log('Filter {' + filter.dataset + '} matches queryScope {' + qs + '}');
         }
         return filterMatchesQueryScope;
       });
@@ -45,5 +44,4 @@
   {:else}
     <Explorer tourConfig={authTour} />
   {/if}
-
 </Content>
