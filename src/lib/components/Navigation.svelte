90fb923c273c191b0a0b940939793b18c6db4a8a
James <Jamestp19@gmail.com>
2024-10-01 17:54:00 -0400

    try 3

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -37,6 +37,30 @@
   };
 
   $: dropdownPath = '';
+
+  import { beforeNavigate } from '$app/navigation';
+  import { hasInvalidFilter, hasGenomicFilter, hasUnallowedFilter } from '$lib/stores/Filter.ts';
+  import { getModalStore } from '@skeletonlabs/skeleton';
+  import FilterWarning from '$lib/components/FilterWarning.svelte';
+
+  const modalStore = getModalStore();
+
+  beforeNavigate((nav) => {
+    console.log(nav);
+    if ($hasInvalidFilter && nav?.to?.url.pathname.includes('/explorer')) {
+      modalStore.trigger({
+        type: 'component',
+        component: 'modalWrapper',
+        meta: { component: FilterWarning, width: 'w-3/4' },
+      });
+    } else if (($hasGenomicFilter || $hasUnallowedFilter) && nav?.to?.url.pathname.includes('/discover')) {
+      modalStore.trigger({
+          type: 'component',
+          component: 'modalWrapper',
+          meta: { component: FilterWarning, width: 'w-3/4' },
+        });
+      }
+  });
 </script>
 
 <AppBar padding="py-0 pl-2 pr-5" background="bg-surface-50-900-token">
