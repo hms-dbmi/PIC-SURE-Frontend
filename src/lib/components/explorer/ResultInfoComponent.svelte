1c250d19cc1319df269f65058adf34b564614a0f
James <Jamestp19@gmail.com>
2024-10-01 11:42:23 -0400

    [ALS-7472] Info results panel to show info when available

--- src/lib/components/explorer/ResultInfoComponent.svelte
+++ src/lib/components/explorer/ResultInfoComponent.svelte
@@ -18,37 +18,60 @@
       <section data-testid="variable-info" class="flex flex-col w-3/4 p-4">
         <h3 class="text-primary-500">Variable Information</h3>
         <div class="w-full flex flex-row justify-between">
-          <div><span class="font-bold">Name:</span> {searchResultDetail.display}</div>
-          <div><span class="font-bold mb-1">Accession:</span> {searchResultDetail.name}</div>
-          <div><span class="font-bold mb-1">Type:</span> {searchResultDetail.type}</div>
+          {#if searchResultDetail.display}
+            <div><span class="font-bold">Name:</span> {searchResultDetail.display}</div>
+          {/if}
+          {#if searchResultDetail.name}
+            <div><span class="font-bold mb-1">Accession:</span> {searchResultDetail.name}</div>
+          {/if}
+          {#if searchResultDetail.type}
+            <div><span class="font-bold mb-1">Type:</span> {searchResultDetail.type}</div>
+          {/if}
         </div>
-        <div><span class="font-bold">Description:</span> {searchResultDetail.description}</div>
+        {#if searchResultDetail.description}
+          <div><span class="font-bold">Description:</span> {searchResultDetail.description}</div>
+        {/if}
       </section>
       {#if searchResultDetail.table}
         <section data-testid="dataset-info" class="flex flex-col w-3/4 p-4">
           <h3 class="text-primary-500">Dataset Information</h3>
           <div class="w-full flex flex-row justify-between">
-            <div><span class="font-bold mb-1">Name:</span> {searchResultDetail.table.display}</div>
-            <div><span class="font-bold">Accession:</span> {searchResultDetail.table.name}</div>
+            {#if searchResultDetail.table.display}
+              <div>
+                <span class="font-bold mb-1">Name:</span>
+                {searchResultDetail.table.display}
+              </div>
+            {/if}
+            {#if searchResultDetail.table.name}
+              <div><span class="font-bold">Accession:</span> {searchResultDetail.table.name}</div>
+            {/if}
           </div>
-          <div>
-            <span class="font-bold">Description:</span>
-            {searchResultDetail.table.description}
-          </div>
+          {#if searchResultDetail.table.description}
+            <div>
+              <span class="font-bold">Description:</span>
+              {searchResultDetail.table.description}
+            </div>
+          {/if}
         </section>
       {/if}
       {#if searchResultDetail.study}
         <section data-testid="study-info" class="flex flex-col w-3/4 p-4">
           <h3 class="text-primary-500">Study Information</h3>
           <div class="w-full flex flex-col">
-            <div>
-              <span class="font-bold mb-1">Study Name:</span>
-              {searchResultDetail.study.display}
-            </div>
-            <div>
-              <span class="font-bold">Study Accession:</span>
-              {searchResultDetail.study.name}
-            </div>
+            {#if searchResultDetail.study.display || searchResultDetail.study.studyAcronym || searchResultDetail?.studyAcronym}
+              <div>
+                <span class="font-bold mb-1">Study Name:</span>
+                {searchResultDetail.study.display ||
+                  searchResultDetail.study.studyAcronym ||
+                  searchResultDetail?.studyAcronym}
+              </div>
+            {/if}
+            {#if searchResultDetail.study.name}
+              <div>
+                <span class="font-bold">Study Accession:</span>
+                {searchResultDetail.study.name}
+              </div>
+            {/if}
           </div>
         </section>
       {/if}
