4a2f52b0fb187c0e03af6aab2a092af36ec69f12
James <Jamestp19@gmail.com>
2024-10-02 08:53:37 -0400

    move to inner layout

--- src/routes/(picsure)/+layout.svelte
+++ src/routes/(picsure)/+layout.svelte
@@ -53,6 +53,28 @@
     !$page.url.pathname.includes('/export') &&
     !$page.url.pathname.includes('/distributions');
 
+
+    import { beforeNavigate } from '$app/navigation';
+  import { hasInvalidFilter, hasGenomicFilter, hasUnallowedFilter } from '$lib/stores/Filter.ts';
+  import FilterWarning from '$lib/components/FilterWarning.svelte';
+
+  beforeNavigate((nav) => {
+    console.log(nav);
+    if ($hasInvalidFilter && nav?.to?.url.pathname.includes('/explorer')) {
+      modalStore.trigger({
+        type: 'component',
+        component: 'modalWrapper',
+        meta: { component: FilterWarning, width: 'w-3/4' },
+      });
+    } else if (($hasGenomicFilter || $hasUnallowedFilter) && nav?.to?.url.pathname.includes('/discover')) {
+      modalStore.trigger({
+          type: 'component',
+          component: 'modalWrapper',
+          meta: { component: FilterWarning, width: 'w-3/4' },
+        });
+      }
+  });
+
 </script>
 
 <Toast position="t" />
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,32 +8,9 @@
   import GoogleConsents from '$lib/components/tracking/GoogleConsents.svelte';
   import GoogleTagManger from '$lib/components/tracking/GoogleTagManger.svelte';
   let googleTagManagerId = settings.google.tagManager;
-  import { beforeNavigate } from '$app/navigation';
-  import { hasInvalidFilter, hasGenomicFilter, hasUnallowedFilter } from '$lib/stores/Filter.ts';
-  import { getModalStore } from '@skeletonlabs/skeleton';
-  import FilterWarning from '$lib/components/FilterWarning.svelte';
 
-  
   initializeStores();
   initializeBranding();
-  beforeNavigate((nav) => {
-    console.log(nav);
-    if ($hasInvalidFilter && nav?.to?.url.pathname.includes('/explorer')) {
-      const modalStore = getModalStore();
-      modalStore.trigger({
-        type: 'component',
-        component: 'modalWrapper',
-        meta: { component: FilterWarning, width: 'w-3/4' },
-      });
-    } else if (($hasGenomicFilter || $hasUnallowedFilter) && nav?.to?.url.pathname.includes('/discover')) {
-      const modalStore = getModalStore();
-      modalStore.trigger({
-          type: 'component',
-          component: 'modalWrapper',
-          meta: { component: FilterWarning, width: 'w-3/4' },
-        });
-      }
-  });
 </script>
 
 <main class="w-full h-full">
