2895588f8fd5b7b52bf347aa07aa0523487ef079
James <Jamestp19@gmail.com>
2024-10-23 14:40:12 -0400

    load meta

--- src/lib/components/modals/Confirmation.svelte
+++ src/lib/components/modals/Confirmation.svelte
@@ -1,9 +1,32 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
+  import { getModalStore } from '@skeletonlabs/skeleton';
+
+  const modalStore = getModalStore();
+  
   export let message: string;
   export let onCancel: () => void;
   export let onConfirm: () => void;
   export let cancelText: string = 'Cancel';
   export let confirmText: string = 'Accept';
+
+  onMount(() => {
+    if ($modalStore[0]?.meta.onCancel) {
+      onCancel = $modalStore[0]?.meta.onCancel;
+    }
+    if ($modalStore[0]?.meta.onConfirm) {
+      onConfirm = $modalStore[0]?.meta.onConfirm;
+    }
+    if ($modalStore[0]?.meta.confirmText) {
+      confirmText = $modalStore[0]?.meta.confirmText;
+    }
+    if ($modalStore[0]?.meta.cancelText) {
+      cancelText = $modalStore[0]?.meta.cancelText;
+    }
+    if ($modalStore[0]?.meta.message) {
+      message = $modalStore[0]?.meta.message;
+    }
+  });
 </script>
 
 <div class="flex flex-col gap-4">
