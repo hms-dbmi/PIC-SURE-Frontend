70627a167f3c64a56ec1e5505caf3a5f3fa389dd
James <Jamestp19@gmail.com>
2024-11-01 13:14:15 -0400

    [ALS-7781] Error Alert updates

--- src/lib/components/ErrorAlert.svelte
+++ src/lib/components/ErrorAlert.svelte
@@ -2,8 +2,8 @@
   export let title: string;
 </script>
 
-<aside data-testid="error-alert" class="alert variant-filled-error">
-  <i class="fa-solid fa-triangle-exclamation text-4xl" aria-hidden="true"></i>
+<aside data-testid="error-alert" class="alert variant-ghost-error">
+  <i class="fa-solid fa-circle-exclamation text-4xl" aria-hidden="true"></i>
   <div class="alert-message">
     <h3 class="h3 text-left">{title}</h3>
     <slot />
--- src/lib/components/explorer/FacetSideBar.svelte
+++ src/lib/components/explorer/FacetSideBar.svelte
@@ -64,5 +64,7 @@
     </Accordion>
   {/if}
 {:catch}
-  <ErrorAlert title="An error occured while retrieving facets." />
+  <ErrorAlert title="Something went wrong loading your search options.">
+    <p>Please wait a moment, refresh the page, and try again.</p>
+  </ErrorAlert>
 {/await}
