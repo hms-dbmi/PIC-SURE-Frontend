d6a8703e343c51f7b4ed5add3c80bf91b9b93fc6
James <Jamestp19@gmail.com>
2024-11-01 15:26:10 -0400

    [ALS-7781] Update error alert colors (#286)

--- src/lib/components/ErrorAlert.svelte
+++ src/lib/components/ErrorAlert.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
   export let title: string;
+  export let color: string = 'error';
 </script>
 
-<aside data-testid="error-alert" class="alert variant-filled-error">
-  <i class="fa-solid fa-triangle-exclamation text-4xl" aria-hidden="true"></i>
+<aside data-testid="error-alert" class="alert variant-ghost-{color}">
+  <i class="fa-solid fa-circle-exclamation text-4xl" aria-hidden="true"></i>
   <div class="alert-message">
     <h3 class="h3 text-left">{title}</h3>
     <slot />
