be0d0bb51029caa6525c4eee5298aacc49da908f
GeorgeC <gcolon021@gmail.com>
2024-11-05 09:00:33 -0500

    Consolidate tour configuration files
    Merged multiple tour configuration files into a single file to streamline management. Updated Svelte components to dynamically load tour data based on environment variables. This change simplifies configuration and enhances maintainability.

--- src/routes/(picsure)/(authorized)/explorer/+page.svelte
+++ src/routes/(picsure)/(authorized)/explorer/+page.svelte
@@ -2,7 +2,18 @@
   import { branding } from '$lib/configuration';
   import Content from '$lib/components/Content.svelte';
   import Explorer from '$lib/components/explorer/Explorer.svelte';
-  import authTour from '$lib/assets/authTourConfiguration.json';
+  import TourData from '$lib/assets/TourConfiguration.json';
+
+  /* eslint-disable @typescript-eslint/no-explicit-any */
+  const Tour: Record<string, any> = TourData;
+
+  const tourName: string | undefined = import.meta.env.AUTH_TOUR_NAME;
+
+  let authTour: any = undefined;
+  if (tourName !== undefined && tourName in Tour) {
+    authTour = Tour[tourName];
+  }
+
 </script>
 
 <svelte:head>
--- src/routes/(picsure)/(public)/discover/+page.svelte
+++ src/routes/(picsure)/(public)/discover/+page.svelte
@@ -2,7 +2,16 @@
   import Content from '$lib/components/Content.svelte';
   import { branding } from '$lib/configuration';
   import Explorer from '$lib/components/explorer/Explorer.svelte';
-  import openTour from '$lib/assets/openTourConfiguration.json';
+  import TourData from '$lib/assets/TourConfiguration.json';
+
+  /* eslint-disable @typescript-eslint/no-explicit-any */
+  const Tour: Record<string, any> = TourData;
+  const tourName = import.meta.env.OPEN_TOUR_NAME;
+
+  let openTour: any = undefined;
+  if (tourName !== undefined && tourName in Tour) {
+    openTour = Tour[tourName];
+  }
 </script>
 
 <svelte:head>
