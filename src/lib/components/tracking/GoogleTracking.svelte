5ca2f4cf1f8aafcc0d20565265440868f92c931e
James <Jamestp19@gmail.com>
2024-12-17 15:58:23 -0500

    Update GoogleTracking.svelte

--- src/lib/components/tracking/GoogleTracking.svelte
+++ src/lib/components/tracking/GoogleTracking.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-  import { run } from 'svelte/legacy';
-
   import { onMount } from 'svelte';
   import { gtag } from 'gtagjs';
   import { branding, settings } from '$lib/configuration';
@@ -10,9 +8,8 @@
   let googleTag = settings.google.tagManager;
   let googleAnalyticsID = settings.google.analytics;
   let googleConsentVisible = $state(false);
-  
 
-  run(() => {
+  $effect(() => {
     if (
       googleAnalyticsID &&
       browser &&
@@ -57,52 +54,41 @@
 
 <svelte:head>
   {#if googleAnalyticsID}
-    <script async src="https://www.googletagmanager.com/gtag/js?id={googleAnalyticsID}"></script>
-    <script data-analyticsID={googleAnalyticsID}>
-      let googleAnalyticsID = document.currentScript.getAttribute('data-analyticsID');
-      window.dataLayer = window.dataLayer || [];
-
-      function gtag() {
-        dataLayer.push(arguments);
-      }
-
-      gtag('js', new Date());
-      gtag('config', googleAnalyticsID);
-
-      gtag('consent', 'default', {
-        ad_storage: 'denied',
-        analytics_storage: 'denied',
-        personalization_storage: 'denied',
-        ad_personalization: 'denied',
-        ad_data: 'denied',
-        ad_user_data: 'denied',
-      });
-
-      let consents = localStorage.getItem('consentMode');
-      if (consents) {
-        consents = JSON.parse(consents);
-        gtag('consent', 'update', consents);
-      }
-    </script>
+    {@html `
+      <script async src="https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsID}"><\/script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag() {
+          dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', '${googleAnalyticsID}');
+        gtag('consent', 'default', {
+          ad_storage: 'denied',
+          analytics_storage: 'denied',
+          personalization_storage: 'denied',
+          ad_personalization: 'denied',
+          ad_data: 'denied',
+          ad_user_data: 'denied',
+        });
+        let consents = localStorage.getItem('consentMode');
+        if (consents) {
+          consents = JSON.parse(consents);
+          gtag('consent', 'update', consents);
+        }
+      <\/script>
+    `}
   {/if}
   {#if googleTag}
-    <script data-googleTag={googleTag}>
-      let googleTag = document.currentScript.getAttribute('data-googleTag');
-
-      (function (w, d, s, l, i) {
-        w[l] = w[l] || [];
-        w[l].push({
-          'gtm.start': new Date().getTime(),
-          event: 'gtm.js',
-        });
-        var f = d.getElementsByTagName(s)[0],
-          j = d.createElement(s),
-          dl = l != 'dataLayer' ? '&l=' + l : '';
-        j.async = true;
-        j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-        f.parentNode.insertBefore(j, f);
-      })(window, document, 'script', 'dataLayer', googleTag);
-    </script>
+    {@html `
+      <script>
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${googleTag}');
+      <\/script>
+    `}
   {/if}
 </svelte:head>
 
