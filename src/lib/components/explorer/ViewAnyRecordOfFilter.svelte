e1cc9be7fd4bda5f88d1f2663fedc9234b147589
James <Jamestp19@gmail.com>
2025-05-21 15:53:12 -0400

    Add modal

---
+++ src/lib/components/explorer/ViewAnyRecordOfFilter.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import type { Filter, AnyRecordOfFilterInterface } from '$lib/models/Filter';
+
+  let { filter }: { filter: Filter } = $props();
+</script>
+
+<div>
+  <header>
+    <h2>
+      Variables in {filter.variableName} category
+    </h2>
+    <h3>
+      {`${(filter as AnyRecordOfFilterInterface)?.concepts?.length} variable(s) in ${filter.variableName} category`}
+    </h3>
+  </header>
+  {#if filter.filterType === 'AnyRecordOf'}
+    {#each filter.concepts as concept}
+      <div>
+        {concept}
+      </div>
+    {/each}
+  {:else}
+    No filter provided
+  {/if}
+</div>
