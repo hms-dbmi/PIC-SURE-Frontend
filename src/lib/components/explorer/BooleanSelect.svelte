6da2dcc735c0fdfc982b433fa86d098eb6feaf51
James <Jamestp19@gmail.com>
2025-06-05 11:54:39 -0400

    Update BooleanSelect.svelte

--- src/lib/components/explorer/BooleanSelect.svelte
+++ src/lib/components/explorer/BooleanSelect.svelte
@@ -4,7 +4,14 @@
   let { value = JoinTypes.AND, onChange }: { value: JoinTypes, onChange: (event: Event) => void } = $props();
 </script>
 
-<select id="boolean-select" class="select w-fit" bind:value={value} onchange={onChange}>
+<select 
+  id="boolean-select" 
+  class="select w-fit" 
+  bind:value={value} 
+  onchange={onChange}
+  onclick={(e) => e.stopPropagation()}
+  onkeydown={(e) => e.stopPropagation()}
+>
   <option value={JoinTypes.AND} selected>And</option>
   <option value={JoinTypes.OR}>Or</option>
 </select>
