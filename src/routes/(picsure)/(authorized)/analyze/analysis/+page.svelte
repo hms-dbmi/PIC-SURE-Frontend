9307ea0218a6a43d455b0f98de0183348e1d3efe
James <Jamestp19@gmail.com>
2025-06-11 11:27:02 -0400

    Create GIC analysis page

---
+++ src/routes/(picsure)/(authorized)/analyze/analysis/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Content from '$lib/components/Content.svelte';
+  import { branding } from '$lib/configuration';
+</script>
+
+<Content backUrl="/analyze" title={`Analyze with ${branding.analysisConfig.analysis.platform}`}>
+  <div class="flex flex-col gap-4 w-full items-center mb-8">
+      {@html branding.analysisConfig.analysis.introduction}
+      {@html branding.analysisConfig.analysis.access}
+      {@html branding.analysisConfig.analysis.examples}
+  </div>
+</Content>
