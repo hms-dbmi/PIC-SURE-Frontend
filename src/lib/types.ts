021ff82bc8e69e13b84ed5f9948896e846e4c4f3
James <Jamestp19@gmail.com>
2025-06-11 11:27:11 -0400

    Update footer

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,9 +1,10 @@
-import type { Column } from './models/Tables';
+import type { Column } from '$lib/components/datatable/types';
 
 interface Link {
   title: string;
   url: string;
   newTab?: boolean;
+  feature?: string;
 }
 
 export type Indexable = {
@@ -17,6 +18,7 @@ export interface Indexed<T> {
 
 export interface SiteMapConfig {
   category: string;
+  feature?: string;
   privilege: string;
   links: Array<Link>;
 }
@@ -106,13 +108,21 @@ export interface PrivacyConfig {
 }
 
 export interface AnalysisConfig {
-  cards: Array<{
-    header: string;
-    body: string;
-    link: string;
-  }>;
-  instructions: {
-    connection: string;
-    execution: string;
+  api: {
+    cards: Array<{
+      header: string;
+      body: string;
+      link: string;
+    }>;
+    instructions: {
+      connection: string;
+      execution: string;
+    };
+  };
+  analysis: {
+    platform: string;
+    introduction: string;
+    access: string;
+    examples: string;
   };
 }
