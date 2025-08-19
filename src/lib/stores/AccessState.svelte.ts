8863a163cddab90ac7eeb343f65dfc95e16179c8
James <Jamestp19@gmail.com>
2025-08-19 12:37:50 -0400

    Update AccessState.svelte.ts

--- src/lib/stores/AccessState.svelte.ts
+++ src/lib/stores/AccessState.svelte.ts
@@ -2,11 +2,12 @@ import { readable } from 'svelte/store';
 import { page } from '$app/state';
 import { features } from '$lib/configuration';
 import { isUserLoggedIn } from '$lib/stores/User';
+import { browser } from '$app/environment';
 
 export const useAuth = readable(!features.login.open || isUserLoggedIn());
 
 export const openUsersOnly = readable(
-  page.url.pathname.includes('/discover') || (!isUserLoggedIn() && features.explorer.open),
+  (browser && page.url.pathname.includes('/discover')) || (!isUserLoggedIn() && features.explorer.open),
 );
 
 export const authorizedUsersOnly = readable(isUserLoggedIn() && !features.explorer.open);
