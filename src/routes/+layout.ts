c630231d1c1d4b2aa064ad4378c06ef740300954
James <Jamestp19@gmail.com>
2025-08-26 10:59:54 -0400

    add redirect

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,9 +1,13 @@
 import type { LayoutLoad } from './$types';
 import { redirect } from '@sveltejs/kit';
 import { browser } from '$app/environment';
+import { features } from '$lib/configuration';
 
 export const load: LayoutLoad = async ({ url, fetch }) => {
   if (browser) {
+    if (!features.login.open && !localStorage.getItem('token')) {
+      redirect(302, '/login');
+    }
     try {
       const response = await fetch(url.pathname);
       if (response.status === 404) {
