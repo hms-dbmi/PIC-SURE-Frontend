032ef588c468923a22aaa0259399ca237e65f7d4
James <Jamestp19@gmail.com>
2025-09-05 09:34:50 -0400

    Allow help popover/icon to be configured

--- src/lib/components/HelpInfoPopup.svelte
+++ src/lib/components/HelpInfoPopup.svelte
@@ -1,27 +1,29 @@
 <script lang="ts">
   import Popover from '$lib/components/Popover.svelte';
+  import { sanitizeHTML } from '$lib/utilities/HTML';
   import type { Snippet } from 'svelte';
 
   interface Props {
     text?: string;
     id?: string;
+    type?: 'question' | 'info' | 'exclamation' | 'xmark';
+    color?: string;
     children?: Snippet;
   }
 
-  const { text, id = '', children }: Props = $props();
+  const { text, id = '', type = 'question', color = 'primary', children }: Props = $props();
 </script>
 
 {#if text || children}
   <div data-testid={id}>
     <Popover data-testid="{id}-content" triggerTypes={['click', 'hover']}>
       {#snippet trigger()}
-        <i class="fa-solid fa-circle-question text-primary-800-200 hover:text-secondary-800-200"
-        ></i>
+        <i class="fa-solid fa-circle-{type} text-{color}-800-200 hover:text-{color}-800-200"></i>
       {/snippet}
       {#if children}
         {@render children?.()}
       {:else if text}
-        {text}
+        {@html sanitizeHTML(text)}
       {/if}
     </Popover>
   </div>
