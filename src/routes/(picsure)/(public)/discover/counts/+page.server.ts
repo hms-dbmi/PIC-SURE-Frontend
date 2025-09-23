972a2733cda13b6cc3602fcc20451e5c963c2a08
James <Jamestp19@gmail.com>
2025-09-23 11:26:50 -0400

    try localhost

--- src/routes/(picsure)/(public)/discover/counts/+page.server.ts
+++ src/routes/(picsure)/(public)/discover/counts/+page.server.ts
@@ -29,8 +29,8 @@ export const load: PageServerLoad<{ studies: CleanedStudyData[] }> = async ({ ur
     };
     if (!cachedData) {
       const [drawerRes, dashboardRes] = await Promise.all([
-        fetch(`${url.origin}/${Picsure.DashboardDrawer}`, options),
-        fetch(`${url.origin}/${Picsure.Dashboard}`, options),
+        fetch(`https://localhost/${Picsure.DashboardDrawer}`, options),
+        fetch(`https://localhost/${Picsure.Dashboard}`, options),
       ]);
 
       if (!drawerRes.ok) throw new Error(`Drawer request failed: ${drawerRes.status}`);
