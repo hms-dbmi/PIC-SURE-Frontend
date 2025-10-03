7f759c9a0362be77ee7e32416273206d0e97774a
James <Jamestp19@gmail.com>
2025-10-03 14:23:59 -0400

    [ALS-9948] Add Timeseries export option (#544)

--- src/lib/components/explorer/export/TypeStep.svelte
+++ src/lib/components/explorer/export/TypeStep.svelte
@@ -2,24 +2,38 @@
   import Summary from '$lib/components/explorer/export/Summary.svelte';
   import CardButton from '$lib/components/buttons/CardButton.svelte';
   import { getActiveType, setActiveType } from '$lib/ExportStepperManager.svelte';
+  import { features } from '$lib/configuration';
 </script>
 
 <section class="flex flex-col w-full h-full items-center">
   <Summary />
-  <div class="grid gap-10 grid-cols-2">
+  <div class="flex flex-row justify-center">
     <CardButton
       data-testid="csv-export-option"
       title="Export as Data Frame or CSV"
       subtitle="Export data as a Python or R data frame or a comma-separated values file"
       size="other"
+      class="flex-1 max-w-64"
       active={getActiveType() === 'DATAFRAME'}
       onclick={() => setActiveType('DATAFRAME')}
     ></CardButton>
+    {#if features.explorer.enableExportTimeseries}
+      <CardButton
+        data-testid="timeseries-export-option"
+        title="Export as Timeseries"
+        subtitle="Export as an R or Python data frame or a comma-separated values file including timestamps"
+        size="other"
+        class="flex-1 max-w-64"
+        active={getActiveType() === 'DATAFRAME_TIMESERIES'}
+        onclick={() => setActiveType('DATAFRAME_TIMESERIES')}
+      ></CardButton>
+    {/if}
     <CardButton
       data-testid="pfb-export-option"
       title="Export as PFB"
       subtitle="Export data in Portable Format for Biomedical Data file format"
       size="other"
+      class="flex-1 max-w-64"
       active={getActiveType() === 'DATAFRAME_PFB'}
       onclick={() => setActiveType('DATAFRAME_PFB')}
     ></CardButton>
