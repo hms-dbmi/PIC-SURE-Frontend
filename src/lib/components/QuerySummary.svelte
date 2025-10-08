8238f87eb9b8464b7b6d6e41142a3c77a9c34ee0
Samantha Piatt <samantha.piatt@childrens.harvard.edu>
2025-10-08 10:36:07 -0400

    [ALS-9941] Add json printing for any record of multi and others (#548)

--- src/lib/components/QuerySummary.svelte
+++ src/lib/components/QuerySummary.svelte
@@ -7,6 +7,14 @@
   }
 
   let { query = {} }: Props = $props();
+
+  const fieldList = Object.entries({
+    fields: query?.fields,
+    requiredFields: query?.requiredFields,
+    anyRecordOf: query?.anyRecordOf,
+    anyRecordOfMulti: query?.anyRecordOfMulti?.flat(),
+    crossCountFields: query?.crossCountFields,
+  });
 </script>
 
 <section id="detail-filters-container" class="m-3">
@@ -20,6 +28,17 @@
 <section id="detail-variables-container" class="m-3">
   <p class="font-bold text-left my-1">Additional Variables Included in Dataset</p>
   <ul data-testid="dataset-summary-variables" class="primary-list">
-    <li>query.fields: {JSON.stringify(query?.fields)}</li>
+    {#each fieldList as [key, fields]}
+      <li>
+        {key}:
+        {#if fields}
+          <ul class="ml-4">
+            {#each fields as field}
+              <li>{field.split('\\').filter(Boolean).join(' > ')}</li>
+            {/each}
+          </ul>
+        {/if}
+      </li>
+    {/each}
   </ul>
 </section>
