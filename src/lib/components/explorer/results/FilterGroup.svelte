3ecd8fbd7144c279cf6fa002bf37724e751dca8a
James <Jamestp19@gmail.com>
2025-11-13 11:35:20 -0500

    remove placehodler width

--- src/lib/components/explorer/results/FilterGroup.svelte
+++ src/lib/components/explorer/results/FilterGroup.svelte
@@ -88,7 +88,7 @@
         </div>
       {/if}
       <div
-        class={`w-[250px] filter-group-${group.operator.toLowerCase()}`}
+        class={`filter-group-${group.operator.toLowerCase()}`}
         data-testid={`filter-group-${id}`}
       >
         {#each group.children as child, index (child.uuid)}
