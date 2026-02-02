476496583533b4cb6db95bb14826343cd0efd932
James <Jamestp19@gmail.com>
2026-02-02 09:27:44 -0500

    Add alert banner for government funding notice
    Added an alert banner to inform users about potential government funding issues affecting website operations.

--- src/routes/(authentication)/login/+layout.svelte
+++ src/routes/(authentication)/login/+layout.svelte
@@ -25,6 +25,21 @@
 <Toaster {toaster} />
 <div class="w-full full-height">
   <Dots class="top-dots" />
+  <div class="alert-banner">
+    <p>
+      Because of a lapse in government funding, the information on this website may not be up to date,
+      transactions submitted via the website may not be processed, and the agency may not be able to
+      respond to inquiries until appropriations are enacted. The NIH Clinical Center (the research
+      hospital of NIH) is open. For more details about its operating status, please visit <a
+        href="http://cc.nih.gov/"
+        class="anchor"
+        target="_blank"
+        rel="noopener noreferrer">cc.nih.gov.</a
+      >
+      Updates regarding government operating status and resumption of normal operations can be found at
+      <a href="http://opm.gov/" class="anchor" target="_blank" rel="noopener noreferrer">opm.gov.</a>
+    </p>
+  </div>
   {@render children?.()}
   <Dots class="bottom-dots" />
   <div class="footer">
@@ -33,6 +48,21 @@
 </div>
 
 <style>
+  .alert-banner {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: flex-start;
+    position: relative;
+    border-style: solid;
+    border-image: initial;
+    border-width: 0px 0px 6px;
+    padding: 0.5rem 1rem 0.5rem 2rem;
+    gap: 1rem;
+    color: rgb(0, 0, 0);
+    background: var(--color-error-50);
+    border-color: var(--color-error-500);
+  }
   .full-height {
     height: calc(100% - 56px);
   }
